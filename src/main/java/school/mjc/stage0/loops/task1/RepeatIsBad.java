436ac3f6e0e4006db284622726e6f11af7fe7660
Нодир Асомов <nasomov93@gmail.com>
2023-10-22 04:42:09 +0500

    hi

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,54 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
+        System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
+
+    }
+
+    public static void main(String[] args) {
+        RepeatIsBad repeatIsBad1=new RepeatIsBad();
+        RepeatIsBad repeatIsBad2=new RepeatIsBad();
+        RepeatIsBad repeatIsBad3=new RepeatIsBad();
+        RepeatIsBad repeatIsBad4=new RepeatIsBad();
+        RepeatIsBad repeatIsBad5=new RepeatIsBad();
+        RepeatIsBad repeatIsBad6=new RepeatIsBad();
+        RepeatIsBad repeatIsBad7=new RepeatIsBad();
+        RepeatIsBad repeatIsBad8=new RepeatIsBad();
+        RepeatIsBad repeatIsBad9=new RepeatIsBad();
+        RepeatIsBad repeatIsBad10=new RepeatIsBad();
+        RepeatIsBad repeatIsBad11=new RepeatIsBad();
+        RepeatIsBad repeatIsBad12=new RepeatIsBad();
+        RepeatIsBad repeatIsBad13=new RepeatIsBad();
+        RepeatIsBad repeatIsBad14=new RepeatIsBad();
+        RepeatIsBad repeatIsBad15=new RepeatIsBad();
+        RepeatIsBad repeatIsBad16=new RepeatIsBad();
+        RepeatIsBad repeatIsBad17=new RepeatIsBad();
+        RepeatIsBad repeatIsBad18=new RepeatIsBad();
+        RepeatIsBad repeatIsBad19=new RepeatIsBad();
+        RepeatIsBad repeatIsBad20=new RepeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+        repeatIsBad2.repeatIsBad();
+        repeatIsBad1.repeatIsBad();
+
+
 
     }
 }
