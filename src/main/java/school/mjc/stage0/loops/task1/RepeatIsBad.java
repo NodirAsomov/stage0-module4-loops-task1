1b9c4ea3a3d016e1a6bf24107c90dc17400494a3
Нодир Асомов <nasomov93@gmail.com>
2023-10-22 05:01:23 +0500

    hi

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,51 +3,16 @@ package school.mjc.stage0.loops.task1;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
-        System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
+    public void repeatIsBad() {
+        for (int n=1; n<=20; n++) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
 
-    }
+            }
+        }
+
 
     public static void main(String[] args) {
-        RepeatIsBad repeatIsBad1=new RepeatIsBad();
-        RepeatIsBad repeatIsBad2=new RepeatIsBad();
-        RepeatIsBad repeatIsBad3=new RepeatIsBad();
-        RepeatIsBad repeatIsBad4=new RepeatIsBad();
-        RepeatIsBad repeatIsBad5=new RepeatIsBad();
-        RepeatIsBad repeatIsBad6=new RepeatIsBad();
-        RepeatIsBad repeatIsBad7=new RepeatIsBad();
-        RepeatIsBad repeatIsBad8=new RepeatIsBad();
-        RepeatIsBad repeatIsBad9=new RepeatIsBad();
-        RepeatIsBad repeatIsBad10=new RepeatIsBad();
-        RepeatIsBad repeatIsBad11=new RepeatIsBad();
-        RepeatIsBad repeatIsBad12=new RepeatIsBad();
-        RepeatIsBad repeatIsBad13=new RepeatIsBad();
-        RepeatIsBad repeatIsBad14=new RepeatIsBad();
-        RepeatIsBad repeatIsBad15=new RepeatIsBad();
-        RepeatIsBad repeatIsBad16=new RepeatIsBad();
-        RepeatIsBad repeatIsBad17=new RepeatIsBad();
-        RepeatIsBad repeatIsBad18=new RepeatIsBad();
-        RepeatIsBad repeatIsBad19=new RepeatIsBad();
-        RepeatIsBad repeatIsBad20=new RepeatIsBad();
+        RepeatIsBad repeatIsBad1 = new RepeatIsBad();
         repeatIsBad1.repeatIsBad();
-        repeatIsBad2.repeatIsBad();
-        repeatIsBad3.repeatIsBad();
-        repeatIsBad4.repeatIsBad();
-        repeatIsBad5.repeatIsBad();
-        repeatIsBad6.repeatIsBad();
-        repeatIsBad7.repeatIsBad();
-        repeatIsBad8.repeatIsBad();
-        repeatIsBad9.repeatIsBad();
-        repeatIsBad10.repeatIsBad();
-        repeatIsBad11.repeatIsBad();
-        repeatIsBad12.repeatIsBad();
-        repeatIsBad13.repeatIsBad();
-        repeatIsBad14.repeatIsBad();
-        repeatIsBad15.repeatIsBad();
-        repeatIsBad16.repeatIsBad();
-        repeatIsBad17.repeatIsBad();
-        repeatIsBad18.repeatIsBad();
-        repeatIsBad19.repeatIsBad();
-        repeatIsBad20.repeatIsBad();
     }
 }
