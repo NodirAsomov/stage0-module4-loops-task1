993d0d499b195d2ad1e3658b29d6b88f421c18cd
Нодир Асомов <nasomov93@gmail.com>
2023-10-22 05:45:46 +0500

    hi

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,24 +3,25 @@ package school.mjc.stage0.loops.task1;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad() {
-        String s1 = "\n"+"writing the same code doesn't have much impact, and it's also time-consuming"+"\n";
-        String s3 = s1 + s1 + s1;
-        String s10 = s3 + s3 + s3 + s1;
-        String s20 = s10 + s10 ;
-        //String s100 = s30 + s30 + s30 + s10;
-        //String s300 = s100 + s100 + s100;
-        //String s1000 = s300 + s300 + s300 + s100;
-        System.out.print(s20);
+    public void repeatIsBad(int n) {
+
+            if(n > 0)
+            {
+                repeatIsBad( n-1);
+                System.out.print( "writing the same code doesn't have much impact, and it's also time-consuming\n");
+            }
+            return;
+        }
         //System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
 
 
-    }
+
+
 
 
 
     public static void main(String[] args) {
         RepeatIsBad repeatIsBad1 = new RepeatIsBad();
-        repeatIsBad1.repeatIsBad();
+        repeatIsBad1.repeatIsBad(20);
     }
 }
